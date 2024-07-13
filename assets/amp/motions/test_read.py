c405f76c20369f5e92cdf4161f9ca3c12ccafbec
apirrone <awanegenebayo@gmail.com>
2024-07-13 20:34:33 +0200

    update

--- assets/amp/motions/test_read.py
+++ assets/amp/motions/test_read.py
@@ -1,7 +1,18 @@
 import numpy as np
 
-data = np.load("amp_humanoid_walk.npy", allow_pickle=True)
-rot = dict(data.item())["rotation"]["arr"]
-# print(len(rot))
-for r in rot:
-    print(r)
+data = dict(np.load("amp_humanoid_walk.npy", allow_pickle=True).item())
+
+# print(data["global_velocity"]["arr"])
+# exit()
+new_data = {}
+new_data["rotation"] = data["rotation"]
+new_data["__name__"] = data["__name__"]
+new_data["root_translation"] = data["root_translation"]
+new_data["global_velocity"] = data["global_velocity"]
+new_data["global_angular_velocity"] = data["global_angular_velocity"]
+new_data["skeleton_tree"] = data["skeleton_tree"]
+np.save("TEST_amp_humanoid_walk.npy", new_data, allow_pickle=True)
+# rot = data["rotation"]["arr"]
+# # print(len(rot))
+# for r in rot:
+#     print(r)
